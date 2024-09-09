ec5702ad67676791ad6ba4d8debca6a09c964115
valium69mg <xdeus.v2@gmail.com>
2024-09-09 16:43:59 -0600

    Add patch function to the API to modify username or password or both

--- src/main/java/com/carlos_spring/tickets_api/users/UserController.java
+++ src/main/java/com/carlos_spring/tickets_api/users/UserController.java
@@ -1,10 +1,15 @@
 package com.carlos_spring.tickets_api.users;
 
+import java.util.Map;
 import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,10 +39,38 @@ public class UserController {
 		return user;
 	}
 	
+	// ONLY WITH API AUTH KEY
 	@GetMapping("/user/{id}")
 	public Optional<User> getUserById(@PathVariable Integer id) {
 		return userRepository.findById(id);
 	}
+	
+	@PatchMapping(value = "/user/{id}")
+	public ResponseEntity<Object> updateUsernameOrPassword(@PathVariable Integer id, @RequestBody Map<String, String> json) {
+		// extract parameters from req
+		String newUsername = json.get("username");
+		String newPassword = json.get("password");
+		
+		// find user
+		Optional<User> userToUpdate = userRepository.findById(id);
+		
+		// a user_id matches path variable id and username and password are present
+		if (userToUpdate.isPresent() & newUsername != null) {
+			User userWithNewUsername = userToUpdate.get();
+			userWithNewUsername.setUsername(newUsername);
+			userRepository.save(userWithNewUsername);
+		}
+		if (userToUpdate.isPresent() & newPassword != null) {
+			User userWithNewPassword = userToUpdate.get();
+			userWithNewPassword.setPassword(newPassword);
+			userRepository.save(userWithNewPassword);
+		}
+		
+		return new ResponseEntity<>(userRepository.findById(id),HttpStatus.ACCEPTED);
+		
+	}
+	
+	
 	
 	public UserRepository getUserRepository() {
 		return userRepository;

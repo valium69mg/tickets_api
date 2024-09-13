c5235d42ab7dfe88dcc37e76a2b7615f3836f57f
valium69mg <xdeus.v2@gmail.com>
2024-09-13 11:53:49 -0600

    Add validation to parameters of 'Artist' entity

--- src/main/java/com/carlos_spring/tickets_api/artists/Artist.java
+++ src/main/java/com/carlos_spring/tickets_api/artists/Artist.java
@@ -3,6 +3,8 @@ package com.carlos_spring.tickets_api.artists;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Artist {
@@ -10,7 +12,10 @@ public class Artist {
 	@Id
 	@GeneratedValue
 	private Integer id;
+	@NotNull(message = "Artist's name must not be null")
+	@Size(min=3,max=30, message = "Artist's name length must be between 3 and 30 characters")
 	private String name;
+	@NotNull(message = "Image's path name must not be null")
 	private String image;
 	
 	public Artist() {
